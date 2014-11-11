be7dcf5a25efc8a432dcb26de0cb9be817351138
Michael Basil <michael@intrications.com>
2014-11-11 08:12:53 +0000

    Don't yet enforce Google Play Services

--- app/src/main/java/nmd/nomad/MainActivity.java
+++ app/src/main/java/nmd/nomad/MainActivity.java
@@ -1,5 +1,6 @@
 package nmd.nomad;
 
+import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -15,6 +16,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -36,6 +39,7 @@ import se.walkercrou.places.Prediction;
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final int REQUEST_CODE_RECOVER_GOOGLE_PLAY_SERVICES = 364783;
     private GooglePlaces googlePlacesClient;
     private Holdr_ActivityMain holdr;
     private Subscription automaticSearchingSubscription;
@@ -54,7 +58,51 @@ public class MainActivity extends ActionBarActivity {
         setupList();
         setupAutomaticSearching();
 
-        startSearchByLocation();
+        if (checkGooglePlayServices()) {
+            startSearchByLocation();
+        }
+    }
+
+    private boolean checkGooglePlayServices() {
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (status != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(status)) {
+//                showErrorDialog(status);
+                Toast.makeText(this, "Google Play Services is not installed. It will be required by a future version of this app.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "This device is not supported by Google Play Services.",
+                        Toast.LENGTH_LONG).show();
+//                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    void showErrorDialog(int code) {
+        GooglePlayServicesUtil.getErrorDialog(code, this,
+                REQUEST_CODE_RECOVER_GOOGLE_PLAY_SERVICES).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_RECOVER_GOOGLE_PLAY_SERVICES:
+                if (resultCode == RESULT_CANCELED) {
+                    Toast.makeText(this, "Google Play Services must be installed.",
+                            Toast.LENGTH_SHORT).show();
+//                    finish();
+                }
+                return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkGooglePlayServices();
     }
 
     @Override

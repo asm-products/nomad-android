dcef234a42b894e03c412a4a195e9eba44f21e18
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-02-25 22:16:06 -0300

    Refactoring ServiceGenarator class using singleton pattern

--- app/src/main/java/nmd/nomad/api/ServiceGenerator.java
+++ app/src/main/java/nmd/nomad/api/ServiceGenerator.java
@@ -9,13 +9,17 @@ import retrofit.client.OkClient;
  * Created by nicolasiensen on 1/28/15.
  */
 public class ServiceGenerator {
+    private static Object service;
+
     public static <S> S createService(Class<S> serviceClass, String baseUrl) {
-        RestAdapter.Builder builder = new RestAdapter.Builder()
-                .setEndpoint(baseUrl)
-                .setClient(new OkClient(new OkHttpClient()));
+        if(service == null) {
+            RestAdapter.Builder builder = new RestAdapter.Builder()
+                    .setEndpoint(baseUrl)
+                    .setClient(new OkClient(new OkHttpClient()));
+            RestAdapter adapter = builder.build();
+            service = adapter.create(serviceClass);
+        }
 
-        RestAdapter adapter = builder.build();
-
-        return adapter.create(serviceClass);
+        return (S) service;
     }
 }
